4a7e75ef394f7eb675d334fd7e3a3a648cf6362d
Michael Shin <Michaelshin.115@gmail.com>
2021-01-27 14:34:25 -0800

    new branch

--- client/src/views/Main.js
+++ client/src/views/Main.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import Post from "../components/Post"
 import SearchAppBar from "../components/SearchAppBar"
+import Sidebar from "../components/Sidebar"
+import Feed from "../components/Feed"
+import "../Main.css"
 
 function Main() {
     return (
         <div>
             <SearchAppBar />
-            <Post />
+            <div className="main">
+                <Sidebar />
+                <Feed />
+
+            </div>
+
+
         </div>
     )
 }
